8c873494f25bdaf25ca18f748249d1f675555f66
Jayu <jayeshhande00@gmail.com>
2022-03-25 14:15:27 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+"use strict";
+const overlay = document.querySelector(".overlay");
+
+const modal = document.querySelector(".modal");
+
+const btnCloseModal = document.querySelector(".close-modal");
+
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+function closeModal() {
+  modal.classList.add("hidden");
+
+  overlay.classList.add("hidden");
+}
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
